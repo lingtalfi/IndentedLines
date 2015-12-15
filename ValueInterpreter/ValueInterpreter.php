874498054cc8139f03b0afe58b5b29571f880f1e
lingtalfi <lingtalfi@gmail.com>
2015-12-15 07:34:11 +0100

    initial commit

---
+++ ValueInterpreter/ValueInterpreter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace IndentedLines\ValueInterpreter;
+
+use Bat\StringTool;
+
+
+/**
+ * ValueInterpreter
+ * @author Lingtalfi
+ * 2015-12-14
+ *
+ */
+class ValueInterpreter implements ValueInterpreterInterface
+{
+
+
+    //------------------------------------------------------------------------------/
+    // IMPLEMENTS ValueInterpreterInterface
+    //------------------------------------------------------------------------------/
+    /**
+     * @return mixed
+     */
+    public function getValue($line)
+    {
+        return StringTool::autoCast($line);
+    }
+
+}
